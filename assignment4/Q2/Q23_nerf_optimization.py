2ba9c77437cfafaf96a80511efe2218ef1600f84
shrudh-i <shrudhi.rs21@gmail.com>
2025-03-26 13:48:49 -0400

    changes to imageio

--- assignment4/Q2/Q23_nerf_optimization.py
+++ assignment4/Q2/Q23_nerf_optimization.py
@@ -318,14 +318,14 @@ def optimize_nerf(
             all_preds = np.stack(all_preds, axis=0)
             all_preds_depth = np.stack(all_preds_depth, axis=0)
             # save the video
-            imageio.mimwrite(
+            imageio.v3.mimwrite(
                 os.path.join(sds.output_dir, "videos", f"rgb_ep_{epoch}.mp4"),
                 all_preds,
                 fps=25,
                 quality=8,
                 macro_block_size=1,
             )
-            imageio.mimwrite(
+            imageio.v3.mimwrite(
                 os.path.join(sds.output_dir, "videos", f"depth_ep_{epoch}.mp4"),
                 all_preds_depth,
                 fps=25,
